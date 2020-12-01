4475177eb49c098abfa66e0efa5c3b8f50a0a087
iandone <ioana@fundapps.co>
2020-12-01 17:35:40 +0000

    Refactor - Day 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,43 +7,45 @@ namespace AdventOfCode
 {
     class Program
     {
-        public static int MaxNumberOfDigits = 4; // 2020
+        public static int MaxNumberOfDigits = 4; // 2020.Length
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! This is Alina's Advent of Code console application");
+            Console.WriteLine("Hello World! This is Li's Advent of Code console application");
 
-            string input = File.ReadAllText("input.txt");
+            var input = LoadInput();
 
-            var list = input.Split(null).Where(x => !string.IsNullOrEmpty(x));
+            int comparison = 0;
 
-            int comparisonCount = 0;
-
-            for (int i = 0; i < list.Count(); i++)
+            for (int i = 0; i < input.Count(); i++)
             {
-                int a = int.Parse(list.ElementAt(i));
+                int a = int.Parse(input.ElementAt(i));
 
-                for (int j = 0; j < list.Count() && j != i; j++)
+                for (int j = 0; j < input.Count() && j != i; j++)
                 {
-                    int b = int.Parse(list.ElementAt(j));
+                    int b = int.Parse(input.ElementAt(j));
 
-                    for (int k = 0; k < list.Count() && k != i && k != j; k++)
+                    for (int k = 0; k < input.Count() && k != i && k != j; k++)
                     {
-                        if (list.ElementAt(k).Length <= MaxNumberOfDigits - (a + b).ToString().Length + 3)
+                        if (input.ElementAt(k).Length <= MaxNumberOfDigits - (a + b).ToString().Length + 3)
                         {
-                            int c = int.Parse(list.ElementAt(k));
+                            int c = int.Parse(input.ElementAt(k));
 
-                            comparisonCount++;
+                            comparison++;
 
                             if (a + b + c == 2020)
                             {
-                                Console.WriteLine(
-                                    $"We found combination {a} + {b} + {c} = 2020, multiplied {a * b * c}, after {comparisonCount} comparisons");
+                                Console.WriteLine($"We found combination {a} + {b} + {c} = 2020, multiplied {a * b * c}, after {comparison} comparisons");
                             }
                         }
                     }
                 }
             }
+        }
+
+        private static IEnumerable<string> LoadInput()
+        {
+            return File.ReadAllText("input.txt").Split(null).Where(x => !string.IsNullOrEmpty(x));
         }
     }
 }
