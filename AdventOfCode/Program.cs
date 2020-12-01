a7cf5f8d0c8585b1d8a036490a8c4f1201522504
iandone <ioana@fundapps.co>
2020-12-01 13:43:47 +0000

    Day 1 - Part 1
    --- Day 1: Report Repair ---
    After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
    
    The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
    
    To save your vacation, you need to get all fifty stars by December 25th.
    
    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
    
    Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
    
    Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
    
    For example, suppose your expense report contained the following:
    
    1721
    979
    366
    299
    675
    1456
    In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
    
    Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
    
    Your puzzle answer was 1010884.
    
    The first half of this puzzle is complete! It provides one gold star: *

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        public static int MaxNumberOfDigits = 4;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is Alina's Advent of Code console application");
+
+            string input = File.ReadAllText("input.txt");
+
+            var list = input.Split(null).Where(x => !string.IsNullOrEmpty(x));
+
+            int comparisonCount = 0;
+
+            for (int i = 0; i < list.Count(); i++)
+            { 
+                int a = int.Parse(list.ElementAt(i));
+
+                for (int j = 0; j < list.Count() && j != i; j++)
+                {
+                    if (list.ElementAt(j).Length <= MaxNumberOfDigits - list.ElementAt(i).Length + 3)
+                    {
+                        int b = int.Parse(list.ElementAt(j));
+
+                        comparisonCount++;
+
+                        if (a + b == 2020)
+                        {
+                            Console.WriteLine($"We found pair {a} + {b} = 2020, multiplied {a * b}, after {comparisonCount} comparisons");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
