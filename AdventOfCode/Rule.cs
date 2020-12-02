56f2b7eca3c4f0fd176e25d21f00bd58081079b8
iandone <ioana@fundapps.co>
2020-12-02 17:58:15 +0000

    Day 2 - Refactor 2

---
+++ AdventOfCode/Rule.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Rule
+    {
+        private readonly string _positioning;
+        private readonly string _password;
+
+        public Rule(string input)
+        {
+            string[] ruleParts = input.Split(":");
+            _positioning = ruleParts[0];
+            _password = ruleParts[1];
+        }
+
+        private int RuleCharacter => _positioning[^1];
+
+        private IEnumerable<int> Positioning => Regex.Split(_positioning, "\\D+").Where(p => !string.IsNullOrEmpty(p)).Select(p => int.Parse(p));
+
+        private bool Compare(int position) => _password.ToCharArray()[position] == RuleCharacter;
+
+        public bool IsValid() => Positioning.Sum(position => Compare(position) ? 1 : 0) == 1;
+    }
+}
