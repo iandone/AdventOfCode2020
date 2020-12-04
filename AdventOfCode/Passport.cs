b75bb77fa0dba1971125af2a19a4059aea829899
iandone <ioana@fundapps.co>
2020-12-04 21:59:11 +0000

    Parts 1 & 2
    The answers were 208 & 167 respectively

---
+++ AdventOfCode/Passport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Passport
+    {
+        private readonly Dictionary<string, string> _fields;
+
+        public Passport(string passportFields)
+        {
+            _fields = passportFields.Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
+                .Where(f => f.Contains(":"))
+                .ToDictionary(k => k.Split(":")[0], v => v.Split(":")[1]);
+        }
+
+        private int MinNumOfRulesToApply => _fields.ContainsKey("cid") ? Rules.Definitions.Count : Rules.Definitions.Count - 1;
+
+        public bool IsValid()
+        {
+            if (_fields.Count < MinNumOfRulesToApply)
+            {
+                return false;
+            }
+
+            return _fields.Sum(f => Rules.Evaluate(f.Key, f.Value) ? 1 : 0) == MinNumOfRulesToApply;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,63 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode
 {
     class Program
     {
-        private static readonly Grid _grid = new Grid(Load());
-
         static void Main()
         {
             Console.WriteLine("Hello World! This is Li's Advent of Code console app");
 
-            var results = new List<int>
-            {
-                GetResults(1, 1),
-                GetResults(3, 1),
-                GetResults(5, 1),
-                GetResults(7, 1),
-                GetResults(1, 2)
-            };
-
-            long product = 1;
-            foreach (var result in results)
-            {
-                product *= result;
-            }
-
-            Console.WriteLine($"\nProduct result = {product}");
+            Console.WriteLine($"Number of valid passports: {Load().Sum(p => p.IsValid() ? 1 : 0)}");
         }
 
-        public static int GetResults(int right, int down)
+        private static IList<Passport> Load()
         {
-            int result = _grid.Traverse(right, down);
-            Console.WriteLine($"Result for ({right},{down}): {result}");
-            return result;
-        }
-
-        private static IDictionary<int, string> Load()
-        {
-            var dictionary = new Dictionary<int, string>();
-
-            var file = new StreamReader("input.txt");
-            string line;
-            int index = 1;
-
-            Console.WriteLine("Input:");
-            while ((line = file.ReadLine()) != null)
-            {
-                Console.WriteLine($"{index,3}: {line}");
-                dictionary.Add(index, line);
-                index++;
-            }
-
-            Console.WriteLine();
-
-            file.Close();
-
-            return dictionary;
+            return File.ReadAllText("input.txt").Split("\r\n\r\n").Select(p => new Passport(p)).ToList();
         }
     }
 }
