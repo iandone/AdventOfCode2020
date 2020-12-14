d50adcd6a6b7d5b350e69c6b2b02dd2db6310aa5
iandone <ioana@fundapps.co>
2020-12-14 02:59:26 +0000

    Cleaning up

---
+++ AdventOfCode.Day13/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day13
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World! This is Li's Advent of Code console app");
+
+            // P1
+            var testScenario1 = Load("input-test.txt");
+            Console.WriteLine($"Test 1: ({testScenario1.Part1Result})"); //295
+
+            var realScenario1 = Load("input.txt");
+            Console.WriteLine($"Part 1: ({realScenario1.Part1Result})"); // 2045
+
+            // P2
+            var testScenario2 = Load("input-test.txt"); ;
+            Console.WriteLine($"Test 2: ({testScenario2.Part2Result()})"); // 1068781
+
+            var realScenario2 = Load("input.txt");
+            Console.WriteLine($"Part 2: ({realScenario2.Part2Result()})"); // 402251700208309
+        }
+
+        private static Scenario Load(string filename)
+        {
+            var input = File.ReadAllLines(filename).Where(line => !string.IsNullOrEmpty(line)).ToList();
+
+            var busIds =  new List<int>();
+
+            foreach (string entry in input[1].Split(",", StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (int.TryParse(entry, out int id))
+                {
+                    busIds.Add(id);
+                }
+                else
+                {
+                    // this shall serve us well
+                    busIds.Add(0);
+                }
+            }
+
+            return new Scenario(earliestDepartureTime: int.Parse(input[0]), busIds: busIds);
+        }
+    }
+
+    public class Scenario
+    {
+        private readonly int _earliestDepartureTime;
+        private readonly List<int> _busIds;
+        private readonly Dictionary<int, int> _waitTimes;
+
+        public Scenario(int earliestDepartureTime, List<int> busIds)
+        {
+            _earliestDepartureTime = earliestDepartureTime;
+            _busIds = busIds;
+            _waitTimes = GetWaitTimes();
+        }
+
+        public long Part1Result => EarliestBusId * _waitTimes[EarliestBusId];
+
+        public long Part2Result()
+        {
+            long time = 0;
+
+            long step = _busIds.First();
+            int offsetIndex = 1;
+
+            while (offsetIndex < _busIds.Count)
+            {
+                if (_busIds[offsetIndex] > 0)
+                {
+                    time += step;
+
+                    if ((time + offsetIndex) % _busIds[offsetIndex] > 0)
+                    {
+                        // should divide perfectly
+                        continue;
+                    }
+                    step *= _busIds[offsetIndex];
+                }
+
+                offsetIndex++;
+            }
+
+            return time;
+        }
+
+        private int EarliestBusId => _waitTimes.Where(r => r.Value == _waitTimes.Values.Min()).Single().Key;
+
+        private Dictionary<int, int> GetWaitTimes() => _busIds.Where(id => id > 0).ToDictionary(k => k,
+            v => _earliestDepartureTime - (_earliestDepartureTime % v) + v - _earliestDepartureTime);
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,80 +1,12 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 
 namespace AdventOfCode
 {
     class Program
     {
-        private static readonly int _preamble = 25;
-        private static long[] _input;
-        private static long[] _checklist;
-
         static void Main()
         {
-            Load();
-
-            long p1Result = FindWeakness();
-            Console.WriteLine($"P1 = ({(p1Result > 0 ? p1Result.ToString() : "No such number found")})"); // 36845998
-
-            long p2Result = FindEncryptionWeakness(p1Result);
-            Console.WriteLine($"P2 = ({p2Result})"); // 4830226
-        }
-
-        private static long FindWeakness()
-        {
-            for (int i = 0; i < _checklist.Length; i++)
-            {
-                if (!IsSumOfTwoElements(_checklist[i], _input[i..(i + _preamble)]))
-                {
-                    return _checklist[i];
-                }
-            }
-
-            return -1;
-        }
-
-        private static bool IsSumOfTwoElements(long number, long[] range)
-        {
-            for (int i = 0; i < range.Length; i++)
-            {
-                for (int j = 0; j < range.Length && i != j; j++)
-                {
-                    if (number == range[i] + range[j])
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        private static long FindEncryptionWeakness(long number)
-        {
-            for (int i = 0; i < _checklist.Length; i++)
-            {
-                int j = i;
-
-                while (_checklist[i..j].Sum() < number && j < _checklist.Length)
-                {
-                    j++;
-                }
-
-                if (_checklist[i..j].Sum() == number)
-                {
-                    return _checklist[i..j].Min() + _checklist[i..j].Max();
-                }
-            }
-
-            return -1;
-        }
-
-        private static void Load()
-        {
             Console.WriteLine("Hello World! This is Li's Advent of Code console app");
-            _input = File.ReadAllLines("input.txt").Select(num => long.Parse(num)).ToArray();
-            _checklist = _input[_preamble..];
         }
     }
 }
